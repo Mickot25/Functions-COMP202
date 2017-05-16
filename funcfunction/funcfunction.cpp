b9a4660c130e5470809eadb660daba9ee8e73987
Michael Contreras <michaelcontrerasr25@gmail.com>
2017-05-16 23:46:11 +0800

    nonrepeating

--- funcfunction/funcfunction.cpp
+++ funcfunction/funcfunction.cpp
@@ -3,3 +3,61 @@ Write a program that will produce a table of x and y values for the given functi
 
 y = 3x^5 - 2x^3 + x; x is between a and b in increments of 0.2
 */
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void swapper(int&array1, int&array2)
+{
+	int temp = array1;
+	array1 = array2;
+	array2 = temp;
+}
+
+void sortEXAM(int sortedEXAM[29], int sizeofarray)
+{
+	for (int r = 0; r < sizeofarray - 1; r++) //to compare numbers up to the number of arrays
+	{
+		for (int j = 0; j < sizeofarray - r - 1; j++)
+		{
+			if (sortedEXAM[j] > sortedEXAM[j + 1])
+				swapper(sortedEXAM[j], sortedEXAM[j + 1]);
+		}
+	}
+}
+
+int main()
+{
+	int mode, modecount2, modecount1;
+	bool is_nomode = false;
+	int numbers[30] = { 15,43,25,25,25,25,16,14,93,93,58,14,55,55,55,64,14,43,14,25,15,56,78,13,15,29,14,14,16,100 };
+	sortEXAM(numbers, 30);
+
+	mode = numbers[0];
+	modecount1 = 0;
+	modecount2 = 1; //Any number exists at least once!
+	for (int i = 1; i < 30; ++i)
+	{
+		if (numbers[i] == numbers[i - 1])
+			++modecount2;
+
+		else
+		{
+			if (modecount2 > modecount1)
+			{
+				mode = numbers[i - 1];
+				modecount1 = modecount2;
+			}
+			else if (i != 1 && modecount2 == modecount1)
+			{
+				std::cout << "No mode!\n"; is_nomode = true;
+				break;
+			}
+			modecount2 = 1;
+		}
+	}
+	if (!is_nomode) std::cout << "Mode of these numbers is: " << mode << std::endl;
+
+	_getch();
+	return 0;
+}
---
+++ nonrepeating/nonrepeating.cpp
@@ -0,0 +1,55 @@
+//Array[20] generate random number from 30-50. tapos i sort tapos ipakita yung lowest na nonrepeating.
+//Display yung unsorted at sorted
+
+#include <iostream>
+#include <conio.h>
+#include <time.h>
+using namespace std;
+
+void swap(int&array1, int&array2)
+{
+	int temp;
+	temp = array1;
+	array1 = array2;
+	array2 = temp;
+}
+
+int main()
+{
+	int arr[20] = { 0 }, sortarr[20];
+	int lowest = 0; //in progress
+
+	srand(time(NULL));
+
+	for (int r = 0; r < 20; r++)
+	{
+		arr[r] = 30 + rand() % 21;
+		sortarr[r] = arr[r]; //copy arr to sortarr
+	}
+
+	cout << "Unsorted: " << endl; //Display arr
+	for (int r = 0; r < 20; r++)
+		cout << arr[r] << " ";
+	
+	cout << endl << endl;
+
+	for (int r = 0; r < 19; r++)
+	{
+		for (int j = 0; j < 20 - r - 1; j++)
+		{
+			if (sortarr[j] > sortarr[j + 1])
+				swap(sortarr[j], sortarr[j + 1]);
+		}
+	}
+
+	cout << "Sorted: " << endl; //Display sortarr
+	for (int r = 0; r < 20; r++)
+		cout << sortarr[r] << " ";
+
+	cout << endl << endl;
+
+	cout << "Lowest nonrepeating number: " << lowest << endl; //still in progress at this stage
+
+	_getch();
+	return 0;
+}
